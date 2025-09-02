1c9bf1aadd233c80de6376b46c399aaf4944c580
NI Corp. <74864374+NickNameYouTuber@users.noreply.github.com>
2025-09-02 03:55:44 +0300

    Test

--- newbackend/src/main/java/tech/nicorp/pm/tasks/api/dto/TaskResponse.java
+++ newbackend/src/main/java/tech/nicorp/pm/tasks/api/dto/TaskResponse.java
@@ -1,5 +1,6 @@
 package tech.nicorp.pm.tasks.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,13 +13,21 @@ public class TaskResponse {
     private UUID id;
     private String title;
     private String description;
+    @JsonProperty("column_id")
     private UUID columnId;
+    @JsonProperty("project_id")
     private UUID projectId;
+    @JsonProperty("order")
     private Integer order; // orderIndex
+    @JsonProperty("reviewer_id")
     private UUID reviewerId;
+    @JsonProperty("due_date")
     private OffsetDateTime dueDate;
+    @JsonProperty("estimate_minutes")
     private Integer estimateMinutes;
+    @JsonProperty("created_at")
     private OffsetDateTime createdAt;
+    @JsonProperty("updated_at")
     private OffsetDateTime updatedAt;
 }
 
