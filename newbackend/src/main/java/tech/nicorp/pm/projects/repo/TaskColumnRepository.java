b5c450c5efc3e66d5e8af2f35bc1784bc994d99f
NI Corp. <74864374+NickNameYouTuber@users.noreply.github.com>
2025-10-07 16:54:16 +0300

    Add calls

--- newbackend/src/main/java/tech/nicorp/pm/projects/repo/TaskColumnRepository.java
+++ newbackend/src/main/java/tech/nicorp/pm/projects/repo/TaskColumnRepository.java
@@ -3,9 +3,11 @@ package tech.nicorp.pm.projects.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.nicorp.pm.projects.domain.TaskColumn;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TaskColumnRepository extends JpaRepository<TaskColumn, UUID> {
+    List<TaskColumn> findByProject_IdOrderByOrderIndexAsc(UUID projectId);
 }
 
 
