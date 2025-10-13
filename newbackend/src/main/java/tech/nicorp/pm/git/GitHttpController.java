c7a38612e8ae1bf77006d7be3b51a2dfa5642b5a
NI Corp. <74864374+NickNameYouTuber@users.noreply.github.com>
2025-10-13 21:36:05 +0300

    Update GitHttpController.java

--- newbackend/src/main/java/tech/nicorp/pm/git/GitHttpController.java
+++ newbackend/src/main/java/tech/nicorp/pm/git/GitHttpController.java
@@ -1,17 +1,15 @@
 package tech.nicorp.pm.git;
 
 import lombok.RequiredArgsConstructor;
-import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
-import org.eclipse.jgit.transport.ReceivePack;
 import org.eclipse.jgit.transport.UploadPack;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -34,15 +32,22 @@ public class GitHttpController {
         }
 
         try (Repository repo = new FileRepositoryBuilder().setGitDir(repoPath.toFile()).build()) {
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            
             if ("git-upload-pack".equals(service)) {
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                
+                String serviceName = "# service=git-upload-pack\n";
+                out.write(String.format("%04x", serviceName.length() + 4).getBytes());
+                out.write(serviceName.getBytes());
+                out.write("0000".getBytes());
+                
                 UploadPack uploadPack = new UploadPack(repo);
+                uploadPack.setBiDirectionalPipe(false);
                 uploadPack.sendAdvertisedRefs(new org.eclipse.jgit.transport.RefAdvertiser.PacketLineOutRefAdvertiser(
                     new org.eclipse.jgit.transport.PacketLineOut(out)));
                 
                 HttpHeaders headers = new HttpHeaders();
-                headers.setContentType(MediaType.parseMediaType("application/x-git-upload-pack-advertisement"));
+                headers.add("Content-Type", "application/x-git-upload-pack-advertisement");
+                headers.add("Cache-Control", "no-cache");
                 return new ResponseEntity<>(out.toByteArray(), headers, HttpStatus.OK);
             }
             
@@ -50,7 +55,7 @@ public class GitHttpController {
         }
     }
 
-    @PostMapping(value = "/{repoId}.git/git-upload-pack", consumes = "application/x-git-upload-pack-request")
+    @PostMapping(value = "/{repoId}.git/git-upload-pack")
     public ResponseEntity<byte[]> uploadPack(
             @PathVariable("repoId") String repoId,
             @RequestBody byte[] body) throws IOException {
@@ -63,12 +68,15 @@ public class GitHttpController {
         try (Repository repo = new FileRepositoryBuilder().setGitDir(repoPath.toFile()).build()) {
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             UploadPack uploadPack = new UploadPack(repo);
-            uploadPack.upload(new java.io.ByteArrayInputStream(body), out, null);
+            uploadPack.setBiDirectionalPipe(false);
+            uploadPack.upload(new ByteArrayInputStream(body), out, null);
             
             HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.parseMediaType("application/x-git-upload-pack-result"));
+            headers.add("Content-Type", "application/x-git-upload-pack-result");
+            headers.add("Cache-Control", "no-cache");
             return new ResponseEntity<>(out.toByteArray(), headers, HttpStatus.OK);
         }
     }
 }
+
 
